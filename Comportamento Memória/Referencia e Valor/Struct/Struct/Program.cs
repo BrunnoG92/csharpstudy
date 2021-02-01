294a9a844b7bc11c78f8d057eccf34c825d5483b
Bruno <BrunoG96@outlook.com>
2021-02-01 19:04:59 -0300

    Aula 66 Tipos de Referencia e valor

---
+++ Comportamento Memória/Referencia e Valor/Struct/Struct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p; // Novo struct tipo p. Iniciando com valores pre definidos
+            p.X = 10;
+            p.Y = 5;
+            Console.WriteLine(p);
+            p = new Point(); // Iniciada a struct. Valores inicias zeros
+            Console.WriteLine(p);
+
+        }
+    }
+}
