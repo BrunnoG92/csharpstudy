4a79ff64121b49a66e507b1c8f804815313762ad
Bruno <BrunoG96@outlook.com>
2021-02-01 19:53:49 -0300

    Aula 69 - VetorP1

---
+++ Vetores/Media Altura - P1/Media Altura - P1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Media_Altura___P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o tamanho do vetor");
+            int N = int.Parse(Console.ReadLine());
+            double[] vect = new double[N]; // tipo do vetor [] nomedovetor = new tipodovetor [tamanhodovetor];
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Digite o " + i + "° número do vetor");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double Soma = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                Soma += vect[i];
+            }
+            double avg = Soma / N;
+
+            Console.WriteLine("A média dos números do vetor é:" + avg.ToString("F2", CultureInfo.InvariantCulture));
+;        }
+    }
+}
