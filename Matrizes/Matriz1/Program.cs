7957cfc83ff13f35b52cb65e32b581949fb80998
Bruno <BrunoG96@outlook.com>
2021-02-14 13:24:49 -0300

    Aula79 - Matrizes Ex

---
+++ Matrizes/Matriz1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Matriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+            Console.WriteLine(mat.Length); // tamanho da matriz 
+            Console.WriteLine(mat.Rank); // quantas linhas //
+            Console.WriteLine(mat.GetLength(0)); // dimensão zero tem tamanho 2 (quantidade de linhas )
+            Console.WriteLine(mat.GetLength(1)); //dimensão 1 tem tamanho 3 (Quantidade de colunas )
+        }
+    }
+}
