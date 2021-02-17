1d41e83f17243275908f5b2c85f05b73b73bc0dc
Bruno <BrunoG96@outlook.com>
2021-02-16 22:56:54 -0300

    enum lista de pedidos

---
+++ Enumerações e Composição/Enumerações/Pedidos_Enum/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Pedidos_Enum.Entities.Enums;
+
+namespace Pedidos_Enum.Entities
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return ID
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ Enumerações e Composição/Enumerações/Pedidos_Enum/Program.cs
@@ -0,0 +1,33 @@
+﻿using Pedidos_Enum.Entities;
+using Pedidos_Enum.Entities.Enums;
+using System;
+
+
+namespace Pedidos_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                ID = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PedingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); // TEXTO ENTRE ASPAS IGUAL AO DO ENUM
+            Console.WriteLine(os);
+        }
+        
+    }
+}
+/* ENUM VOCÊ DEFINE OS VALORES POSSIVEIS QUE O ATRIBUTO PODE POSSUIR E ELE PODENDO POSSUI APENAS UM VALOR.
+ * EX: 
+ * CLASSE CONTA POSSUI O ATRIBUTO VENCIMENTO , VENCIMENTO ASSUME VALORES DE DOMINGO À SÁBADO. O ATRIBUTO VENCIMENTO É UM VALOR UNICO
+ * QUE PODE ASSUMIR SOMENTE UM DESSES VALORES (2°,3°,4° ETC) 
