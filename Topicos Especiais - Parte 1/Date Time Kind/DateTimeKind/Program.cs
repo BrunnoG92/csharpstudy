2d066266f71f385352bbb5a9ae103bfc1d8a6829
Bruno <BrunoG96@outlook.com>
2021-02-16 22:21:18 -0300

    Kind e padrao 8601

---
+++ Topicos Especiais - Parte 1/Date Time Kind/DateTimeKind/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DateTimeKind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime D1 = new DateTime(2000, 8, 15, 13, 5, 58, System.DateTimeKind.Local);
+            DateTime D2 = new DateTime(2000, 8, 15, 13, 5, 58, System.DateTimeKind.Utc);
+            DateTime D3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+            Console.WriteLine("D1:" + D1);
+            Console.WriteLine("D1 kind " + D1.Kind);
+            Console.WriteLine("D1 Local :" + D1.ToLocalTime ());
+            Console.WriteLine("D1 UTC: " + D1.ToUniversalTime());
+            Console.WriteLine("=========================");
+           
+            Console.WriteLine("D2:" + D2);
+            Console.WriteLine("D2 kind " + D2.Kind);
+            Console.WriteLine("D2 Local :" + D2.ToLocalTime());
+            Console.WriteLine("D2 UTC: " + D2.ToUniversalTime());
+            Console.WriteLine("========================="); 
+           
+            Console.WriteLine("D3 kind " + D3.Kind);
+            Console.WriteLine("D3 Local :" + D3.ToLocalTime());
+            Console.WriteLine("D3 UTC: " + D3.ToUniversalTime());
+            Console.WriteLine("=========================");
+
+            /* DATAS DEVEM SER SALVA NO PADRÃO UTC NO BANCO */
+
+
+
+        }
+    }
+}
---
+++ Topicos Especiais - Parte 1/ISO 8601/ISO8601/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ISO8601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime D1 = DateTime.Parse(" 2020-08-12 12:34:11");
+            DateTime D2 = DateTime.Parse(" 2020-08-12T12:34:11Z"); // PADRÃO UTC 
+
+            Console.WriteLine("D1:" + D1);
+            Console.WriteLine("D1 Kind: " + D1.Kind);
+            Console.WriteLine("D1 para Local: " + D1.ToLocalTime());
+            Console.WriteLine("D1 para UTC: "+ D1.ToUniversalTime());
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("D2: " + D2);
+            Console.WriteLine("D2 Kind: " + D2.Kind);
+            Console.WriteLine("D2 para Local: " + D2.ToLocalTime());
+            Console.WriteLine("D2 para UTC: " + D2.ToUniversalTime());
+            Console.WriteLine("==============================");
+            Console.WriteLine("D2: " + D2.ToString ("yyyy-MMM-ddTHH:mm:ssZ")); // CUIDADO AO USAR. IDEAL É CONVETER PRIMEIRO PARA UTC
+            Console.WriteLine("D2: " + D2.ToUniversalTime().ToString("yyy-MMM-ddTHH:mm:ssZ"));
+
+
+        }
+    }
+}
+
+/* FORMTATO ISO 8601 UTC
+ * yyy-MM-ddTHH:mm:ssZ
+ * Z indica que a data ou hora está em utc
+ */
