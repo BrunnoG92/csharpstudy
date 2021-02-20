a7b62fc7cfabc37096d302d7cc16911a5dea712f
Bruno <BrunoG96@outlook.com>
2021-02-20 19:21:32 -0300

    Ex Composicao - Entitie Depart and Hour

---
+++ Enumerações e Composição/Composição/Contrato_Func/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Contrato_Func.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+
+        }
+        public Department (string name)
+        {
+            Name = name;
+        }
+    }
+}
