50359ae1453c908644292c75c18909dfbce670d9
Bruno <BrunoG96@outlook.com>
2021-02-20 19:42:14 -0300

    class worker , propeties and constructor

--- Enumerações e Composição/Composição/Contrato_Func/Entities/HourContract.cs
+++ Enumerações e Composição/Composição/Contrato_Func/Entities/HourContract.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Contrato_Func.Entities
 {
     class HourContract
---
+++ Enumerações e Composição/Composição/Contrato_Func/Entities/Worker.cs
@@ -0,0 +1,49 @@
+﻿using Contrato_Func.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Contrato_Func.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set;}
+        public Department Department { get; set; } // Composição. Propriedade do tipo departamento
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>(); // Lista de contratos. Um trabalho tem vários contratos.
+
+        public Worker ()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+            // ASSOCIAÇÃO PARA MUITOS NÃO SÃO ADD NO CONSTRUTOR !//
+
+        }
+        public void AddContract (HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract (HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+        public double Income (int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+                return sum;
+            }
+        }
+    }
+}
