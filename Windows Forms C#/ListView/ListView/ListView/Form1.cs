b0313327fb744622fb3cb71f8dd74b7da119569c
Bruno Gonçalves <BrunoG96@outlook.com>
2021-07-15 18:35:35 -0300

    List View - Insert and create

--- Windows Forms C#/ListView/ListView/ListView/Form1.cs
+++ Windows Forms C#/ListView/ListView/ListView/Form1.cs
@@ -15,7 +15,48 @@ namespace ListView
         public Form1()
         {
             InitializeComponent();
+            CriarColunas();
+        }
+        private void CriarColunas()
+        {
+            lvl_ListaNomes.Clear();
+            lvl_ListaNomes.Columns.Insert(0, "Nome:", 250, HorizontalAlignment.Left);
+            lvl_ListaNomes.Columns.Insert(1, "Idade:", 100, HorizontalAlignment.Right);
+        }
+        private void TxtB_CaixaNome_Validating(object sender, CancelEventArgs e)
+        {
+            if (TxtB_CaixaNome.Text.Trim().Length < 3)
+            {
+                MessageBox.Show("Nome inválido");
+                TxtB_CaixaNome.Focus();
+            }
+        }
+        private void TxtB_CaixaIdade_Validating(object sender, CancelEventArgs e)
+        {
+            if (TxtB_CaixaIdade.Text.Trim().Equals(String.Empty)) //se for vazio a caixa
+            {
+                MessageBox.Show("Idade inválida");
+                TxtB_CaixaIdade.Focus();
+            }
+        }
+        private void Btn1_Cadastrar_Click(object sender, EventArgs e)
+        {
+            ListViewItem ItmX = lvl_ListaNomes.Items.Add(TxtB_CaixaNome.Text.Trim()); // criada a lista ItmX
+            ItmX.SubItems.Add(new ListViewItem.ListViewSubItem(null, TxtB_CaixaIdade.Text.Trim())); //Instanciada a lsita ItmX e a sublista
+            TxtB_CaixaNome.Text = String.Empty;
+            TxtB_CaixaIdade.Text = String.Empty;
+        }
+        private void Btn2_Limpar_Click(object sender, EventArgs e)
+        {
+            lvl_ListaNomes.Items.Clear();
         }
 
+        private void TxtB_CaixaNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        
     }
 }
