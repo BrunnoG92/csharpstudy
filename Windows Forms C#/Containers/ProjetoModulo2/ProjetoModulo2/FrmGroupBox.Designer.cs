99068d4a86106d597c3caf87039802a08739a653
Bruno Gonçalves <BrunoG96@outlook.com>
2021-07-20 11:27:04 -0300

    Container projetct

---
+++ Windows Forms C#/Containers/ProjetoModulo2/ProjetoModulo2/FrmGroupBox.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace ProjetoModulo2
+{
+    partial class FrmGroupBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gpb_DadosFornecedor = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Gpb_Produtos = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Gpb_DadosFornecedor.SuspendLayout();
+            this.Gpb_Produtos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Gpb_DadosFornecedor
+            // 
+            this.Gpb_DadosFornecedor.Controls.Add(this.textBox2);
+            this.Gpb_DadosFornecedor.Controls.Add(this.label2);
+            this.Gpb_DadosFornecedor.Controls.Add(this.textBox1);
+            this.Gpb_DadosFornecedor.Controls.Add(this.label1);
+            this.Gpb_DadosFornecedor.Location = new System.Drawing.Point(25, 21);
+            this.Gpb_DadosFornecedor.Name = "Gpb_DadosFornecedor";
+            this.Gpb_DadosFornecedor.Size = new System.Drawing.Size(724, 100);
+            this.Gpb_DadosFornecedor.TabIndex = 0;
+            this.Gpb_DadosFornecedor.TabStop = false;
+            this.Gpb_DadosFornecedor.Text = "Dados do Fornecedor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(341, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Gpb_Produtos
+            // 
+            this.Gpb_Produtos.Controls.Add(this.textBox3);
+            this.Gpb_Produtos.Controls.Add(this.textBox4);
+            this.Gpb_Produtos.Controls.Add(this.label3);
+            this.Gpb_Produtos.Controls.Add(this.label4);
+            this.Gpb_Produtos.Location = new System.Drawing.Point(25, 163);
+            this.Gpb_Produtos.Name = "Gpb_Produtos";
+            this.Gpb_Produtos.Size = new System.Drawing.Size(724, 119);
+            this.Gpb_Produtos.TabIndex = 1;
+            this.Gpb_Produtos.TabStop = false;
+            this.Gpb_Produtos.Text = "Dados do Produto";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(341, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(75, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 23);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmGroupBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Gpb_Produtos);
+            this.Controls.Add(this.Gpb_DadosFornecedor);
+            this.Name = "FrmGroupBox";
+            this.Text = "FrmGroupBox";
+            this.Gpb_DadosFornecedor.ResumeLayout(false);
+            this.Gpb_DadosFornecedor.PerformLayout();
+            this.Gpb_Produtos.ResumeLayout(false);
+            this.Gpb_Produtos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Gpb_DadosFornecedor;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox Gpb_Produtos;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
