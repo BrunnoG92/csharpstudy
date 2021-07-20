ce9cc47152f9a46fafdb6f4d733dd674977dacbd
Bruno Gonçalves <BrunoG96@outlook.com>
2021-07-20 11:41:31 -0300

    Add panel

---
+++ Windows Forms C#/Containers/ProjetoModulo2/ProjetoModulo2/Frm_Painel.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace ProjetoModulo2
+{
+    partial class Frm_Painel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pnl_Imagem = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(32, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(860, 67);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Pnl_Imagem
+            // 
+            this.Pnl_Imagem.BackgroundImage = global::ProjetoModulo2.Properties.Resources.avengers;
+            this.Pnl_Imagem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Pnl_Imagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pnl_Imagem.Location = new System.Drawing.Point(32, 114);
+            this.Pnl_Imagem.Name = "Pnl_Imagem";
+            this.Pnl_Imagem.Size = new System.Drawing.Size(860, 445);
+            this.Pnl_Imagem.TabIndex = 1;
+            // 
+            // Frm_Painel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 569);
+            this.Controls.Add(this.Pnl_Imagem);
+            this.Controls.Add(this.panel1);
+            this.Name = "Frm_Painel";
+            this.Text = "Frm_Painel";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel Pnl_Imagem;
+    }
+}
---
+++ Windows Forms C#/Containers/ProjetoModulo2/ProjetoModulo2/Frm_Painel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjetoModulo2
+{
+    public partial class Frm_Painel : Form
+    {
+        public Frm_Painel()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
